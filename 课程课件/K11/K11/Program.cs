8151a187ba10b332cfd39623a35c81fc1d27de12
Zmrbak <20995658@qq.com>
2022-01-03 11:01:26 +0800

    add code

---
+++ 课程课件/K11/K11/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text;
+
+var username = "u1";
+var password = "p1";
+using (HttpClient httpClient = new HttpClient())
+{
+    httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
+        "Basic",
+        Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"))
+        );
+    var result = httpClient.GetStringAsync("http://192.168.240.77/my-path").GetAwaiter().GetResult();
+    //var result = httpClient.GetStringAsync("http://192.168.240.77/path").GetAwaiter().GetResult();
+    Console.WriteLine(result);
+}
+
+Console.ReadLine();
